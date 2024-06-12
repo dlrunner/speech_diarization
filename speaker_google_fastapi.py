8ec4ee88f830cebaf29a0913f34c06b423e77c49
Kh5oo <keumhyeonoo@gmail.com>
2024-06-12 10:15:44 +0900

    rttm_dirs 폴더 생성 후 파일 저장시킴

--- speaker_google_fastapi.py
+++ speaker_google_fastapi.py
@@ -6,7 +6,6 @@ from pydub import AudioSegment
 import os
 import speech_recognition as sr
 
-start = time.time()
 pipeline = Pipeline.from_pretrained(
   "pyannote/speaker-diarization-3.1",
   use_auth_token="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")
@@ -14,30 +13,33 @@ pipeline = Pipeline.from_pretrained(
 silence_duration = 1000
 some_silence = AudioSegment.silent(duration=silence_duration) 
 
-# user = 'user002'  # 유저 이름
-# org_filename = f"{user}_{round(time.time())}"  # 유저 이름에 타임스탬프 추가
-# org_filepath = os.path.join("source", org_filename)  # 변수에 담은 파일명에 경로추가 -> 경로/파일명
-# output_folder = os.path.join("file_segments", f"{org_filename}_segments")  # 잘라낸 음성파일 새로 저장할 경로
-# seg_filepath = output_folder  # 잘려진 음성파일이 저장된 경로 지정
-
 r = sr.Recognizer()
 
 app = FastAPI()
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
+    
+    start = time.time()
+    
     user = 'user001' # 유저 이름
     org_filename = user + '_' + str(round(time.time())) # 유저 이름에 타임스탬프 추가
     # org_filepath = "source\\" + org_filename  # 변수에 담은 파일명에 경로추가 -> 경로/파일명
     output_folder = "file_segments\\" + org_filename + "_segments\\"    # 잘라낸 음성파일 새로 저장할 경로 -> file_segments/파일명_segments/
     seg_filepath = "file_segments\\" + org_filename + "_segments"    # 잘려진 음성파일이 저장된 경로 지정
+    rttm_dirs = "rttm_dirs"
 
     byte_file = await file.read()
     audio = io.BytesIO(byte_file)
     diarization = pipeline(audio)
 
-    rttm_name =  org_filename + ".rttm"
-    with open(rttm_name, "w") as rttm: # 현재 폴더에 파일명.rttm 으로 저장 -> 경로 새로 추가 및 지정 필요
+    if not os.path.exists(rttm_dirs):
+        os.makedirs(rttm_dirs)
+
+    # rttm_name =  org_filename + ".rttm"
+    rttm_name = os.path.join(rttm_dirs, org_filename + ".rttm")
+    with open(rttm_name, "w") as rttm: # 파일명.rttm 으로 저장
         diarization.write_rttm(rttm)
+
     for turn, _, speaker in diarization.itertracks(yield_label=True):
         print(str(turn.start) + " -- " + str(turn.end) + " -- " + str(turn.duration)+ " -- " + str(turn.overlaps))
     
@@ -64,15 +66,12 @@ async def create_upload_file(file: UploadFile):
         audio2 = audio_segment[seg_start:seg_end]
         audio2.export(audio_segment_file_name, format="wav")
 
-        # 화자별 텍스트 저장을 위한 딕셔너리 초기화
-    speaker_texts = {}
-    # 세그먼트 파일 목록 가져오기
-    file_list = [f for f in os.listdir(seg_filepath) if f.endswith(".wav")]
+    speaker_texts = {}    # 화자별 텍스트 저장을 위한 딕셔너리 초기화
+    file_list = [f for f in os.listdir(seg_filepath) if f.endswith(".wav")]    # 세그먼트 파일 목록 가져오기
     print(file_list)
 
     # 파일 이름에서 nnnn 부분 추출 및 정렬 -> nnnn은 음성파일에서 화자 존재하기 시작한 시간(분,초)
     file_list.sort(key=lambda x: int(x.split('_')[2].split('.')[0]))    # 시간 순서대로 정렬
-    print(file_list)
     
     # 각 파일에 대해 음성 인식 수행
     for file_name in file_list:
