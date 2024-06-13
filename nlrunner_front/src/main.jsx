22c240889e28e6bcac138a379574d71ae66d4a62
penny <abc@def.com>
2024-06-13 18:25:54 +0900

    헤더푸터완료

--- nlrunner_front/src/main.jsx
+++ nlrunner_front/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App3.jsx'
+import App from './App2.jsx'
+import Header from './header.jsx'
+import Footer from './footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
 );
