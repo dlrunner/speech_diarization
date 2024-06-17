79aa62a0c3d8ac8d29ab44d4fa9c303aa8b27958
ryunji <ryunji1990@gmail.com>
2024-06-17 16:35:33 +0900

    주석 추가

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ app.add_middleware(
     , allow_headers=["*"]
 )
 
+
+# 0. /api 경로로 들어오는 요청을 make_audio_api.router, speaker_google_fastapi, diart_download 에서 정의된 엔드포인트로 라우팅합니다.
+
+# 1. make_audio_api: 실시간 녹음 기능을 제공하며, 입력된 음성을 녹음하여 wav 파일로 생성하는 API 라우터를 정의하는 모듈.
 app.include_router(make_audio_api.router, prefix="/api")
 app.include_router(speaker_google_fastapi.router, prefix="/api")
 app.include_router(diart_download.router, prefix="/api")
