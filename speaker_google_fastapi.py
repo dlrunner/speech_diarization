865004fc357fe80b79d9adcbef4d3958d84bf3c8
ryunji <ryunji1990@gmail.com>
2024-06-18 15:35:37 +0900

    시간 출력 부분 추가

--- speaker_google_fastapi.py
+++ speaker_google_fastapi.py
@@ -74,12 +74,12 @@ async def create_upload_file(file: UploadFile):
         line = line.replace('\r','').replace('\n','')
         line_arr = line.split(' ')
 
-        seg_start = int(line_arr[3].replace('.',''))
+        seg_start   = int(line_arr[3].replace('.',''))
         seg_duration = int(line_arr[4].replace('.',''))
         seg_speaker = line_arr[7]
-        seg_end = seg_start + seg_duration
+        seg_end     = seg_start + seg_duration
         
-        audio_segment_file_name = os.path.join(output_folder, seg_speaker + "_" + str(seg_start) + ".wav")
+        audio_segment_file_name = os.path.join(output_folder, seg_speaker + "_" + str(seg_start) + "_" + str(seg_end) + ".wav")
 
         audio2 = audio_segment[seg_start:seg_end]
         audio2.export(audio_segment_file_name, format="wav")
@@ -93,7 +93,9 @@ async def create_upload_file(file: UploadFile):
     
     for file_name in file_list: # 각 파일에 대해 음성 인식 수행
         
-        speaker_id = file_name.split('_')[1]    # 화자 ID 추출 (예: "SPEAKER_00_31.wav"에서 00 추출)
+        speaker_id     = file_name.split('_')[1]    # 화자 ID 추출 (예: "SPEAKER_00_31.wav"에서 00 추출)
+        seg_start_time = int(file_name.split('_')[2].split('.')[0])  # 시작 시간 추출 (ms 단위)
+        seg_end_time   = int(file_name.split('_')[3].split('.')[0])  # 시작 시간 추출 (ms 단위)
         
         if speaker_id not in speaker_texts: # 화자 ID에 해당하는 텍스트 리스트가 없으면 초기화
             speaker_texts[speaker_id] = []        
@@ -103,7 +105,15 @@ async def create_upload_file(file: UploadFile):
         with sr.AudioFile(file_path) as source: # 오디오 파일을 읽고 음성 인식 수행
             audio = r.listen(source)
             try:
-                text = r.recognize_google(audio, language='ko')
+                
+                start_min, start_sec = divmod(seg_start_time / 1000, 60)  # 시작 시간을 분과 초로 변환
+                end_min, end_sec = divmod(seg_end_time / 1000, 60)        # 종료 시간을 분과 초로 변환
+            
+                # 출력 형식에 맞게 시작 시간과 종료 시간을 문자열로 조합
+                start_time_str = f"{int(start_min):02d}:{int(start_sec):02d}"
+                end_time_str = f"{int(end_min):02d}:{int(end_sec):02d}"
+            
+                text = f"{start_time_str} - {end_time_str} : {r.recognize_google(audio, language='ko')}"
                 speaker_texts[speaker_id].append(text)
                 print(f"화자 {speaker_id}: {text}")
             except sr.UnknownValueError:
